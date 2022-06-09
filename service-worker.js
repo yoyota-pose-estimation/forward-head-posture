a70b43a66d94f659dd028468776fc33844457b86
yoyota <yoyota@users.noreply.github.com>
2022-06-09 03:30:09 +0000

    Automated deployment: Thu Jun  9 03:30:09 UTC 2022 73787942751ca25ceaa69be4a857fc1a09f3be44

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/forward-head-posture/precache-manifest.ff951c50155d73d7660c8bd669e6747f.js"
+  "/forward-head-posture/precache-manifest.9b01162ee52afe3c10cb87a708ed98df.js"
 );
 
 self.addEventListener('message', (event) => {
